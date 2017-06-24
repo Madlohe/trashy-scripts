e9580d0bf5254c56c310547d6fc005e27653f880
Sebastian Wolf <wolfx587@umn.edu>
2017-06-24 17:04:42 -0500

    finished again

--- negative-harmony.c
+++ negative-harmony.c
@@ -9,8 +9,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+char sharps[25][3] =
+{ "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
+  "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C" };
+  
+char flats[25][3] =
+{ "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B",
+  "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C" };
+
 int main(int argc, char** argv) {
   if(argc < 2) {
-    printf("Usage: %s KEY NOTE [NOTES...]", argv[0]);
+    printf("Usage: %s KEY NOTE [NOTES...]\n", argv[0]);
+  }
+  
+  int key; //tonal center represented as an index in the globals.
+  int low_mirror; // minor third of key. actual 'mirror' is between m/M3.
+  int i, j;
+  for(i = 0; i < 12, i++) {
+    if(strncmp(sharps[i], argv[1], 2) == 0 ||
+       strncmp(flats[i],  argv[1], 2) == 0) {
+      key = i;
+      low_mirror = i + 4;
+      printf("Key is %s (%d)\n", argv[1], i);
+      break;
+    }
+  }
+  
+  int current; // Current note
+  int diff;
+  for(j = argc-1; j >= 2; j--) {
+    for(i = 0; i < 12, i++) {
+      if(strncmp(sharps[i], argv[j], 2) == 0 ||
+         strncmp(flats[i],  argv[j], 2) == 0) {
+        current = i;
+        diff = current - low_mirror - 1;
+        int to_print = low_mirror - diff;
+        while(to_print > 20) {
+          to_print -= 12;
+        }
+        while(to_print < 0) {
+          to_print += 12;
+        }
+        printf("%s ", sharps[to_print]);
+      }
+    }
   }
 }
